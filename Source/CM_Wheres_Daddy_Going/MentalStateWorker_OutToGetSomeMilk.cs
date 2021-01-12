63baa486bf1a52cbc3fb8a5f9c42ff1ac5890338
NathanHarbour <nathanharbour@gmail.com>
2021-01-12 15:39:13 -0600

    Log messages

--- Source/CM_Wheres_Daddy_Going/MentalStateWorker_OutToGetSomeMilk.cs
+++ Source/CM_Wheres_Daddy_Going/MentalStateWorker_OutToGetSomeMilk.cs
@@ -11,6 +11,7 @@ namespace CM_Wheres_Daddy_Going
     {
         public override bool StateCanOccur(Pawn pawn)
         {
+            Logger.MessageFormat(this, "Checking for {0}", pawn);
             if (pawn == null || pawn.Dead || !pawn.Spawned || pawn.relations == null || pawn.relations.RelatedPawns == null)
                 return false;
 
@@ -19,8 +20,11 @@ namespace CM_Wheres_Daddy_Going
             if (children == null || children.Count == 0)
                 return false;
 
+            Logger.MessageFormat(this, "{0} has {1} children", pawn, children.Count);
+
             int childrenOnMap = children.Where(child => child.Map == pawn.Map).Count();
 
+            Logger.MessageFormat(this, "{0} has {1} children on the map", pawn, childrenOnMap);
             return childrenOnMap > 0;
         }
     }
