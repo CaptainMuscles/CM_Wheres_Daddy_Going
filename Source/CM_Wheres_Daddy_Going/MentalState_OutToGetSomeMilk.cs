3f8f476e52c6f5e5d611406e0e569d1578f24bc0
NathanHarbour <nathanharbour@gmail.com>
2021-01-12 15:40:04 -0600

    Added bad thoughts for family leaving

--- Source/CM_Wheres_Daddy_Going/MentalState_OutToGetSomeMilk.cs
+++ Source/CM_Wheres_Daddy_Going/MentalState_OutToGetSomeMilk.cs
@@ -41,6 +41,65 @@ namespace CM_Wheres_Daddy_Going
             }
         }
 
+        public override void PostStart(string reason)
+        {
+            base.PostStart(reason);
+
+            List<Pawn> relativesOnMap = pawn.relations.PotentiallyRelatedPawns.Where(otherPawn => otherPawn.Map == pawn.Map).ToList();
+
+            // Make sure we don't add double thoughts in case of someone marrying their cousin or ex-mother-in-law or something
+            List<Pawn> checkedPawns = new List<Pawn>();
+            foreach(Pawn relative in relativesOnMap)
+            {
+                if (checkedPawns.Contains(relative))
+                {
+                    Logger.MessageFormat(this, "Already checked relation to {0}", relative);
+                    continue;
+                }
+
+                PawnRelationDef relationDef = relative.GetMostImportantRelation(pawn);
+                if (relationDef == null)
+                {
+                    Logger.MessageFormat(this, "No relation found between {0} and {1}", relative, pawn);
+                    continue;
+                }
+
+                ThoughtDef lostThoughtDef = relationDef.GetGenderSpecificLostThought(pawn);
+                if (lostThoughtDef == null)
+                {
+                    Logger.MessageFormat(this, "No lostThoughtDef found for {0} to {1}", relationDef, pawn);
+                    continue;
+                }
+
+                string lostThoughtName = lostThoughtDef.defName;
+                string leftThoughtName = lostThoughtName.Replace("Lost", "Left");
+
+                if (leftThoughtName == lostThoughtName)
+                {
+                    Logger.MessageFormat(this, "Unable to rename thought: {0}", leftThoughtName);
+                    continue;
+                }
+
+                ThoughtDef leftThoughtDef = DefDatabase<ThoughtDef>.GetNamed(leftThoughtName, false);
+                if (leftThoughtDef == null)
+                {
+                    Logger.MessageFormat(this, "Could not find thought: {0}", leftThoughtName);
+                    continue;
+                }
+
+                if (relative.needs == null || relative.needs.mood == null || relative.needs.mood.thoughts == null || relative.needs.mood.thoughts.memories == null)
+                {
+                    Logger.MessageFormat(this, "Could not find needs/mood/thoughts/memories for {0}", relative);
+                    continue;
+                }
+
+                Logger.MessageFormat(this, "Attempting to add thought {0} to {1}", leftThoughtName, relative);
+                relative.needs.mood.thoughts.memories.TryGainMemory(leftThoughtDef, pawn);
+
+                checkedPawns.Add(relative);
+            }
+        }
+
         public override string GetBeginLetterText()
         {
             string baseLetter = "";
