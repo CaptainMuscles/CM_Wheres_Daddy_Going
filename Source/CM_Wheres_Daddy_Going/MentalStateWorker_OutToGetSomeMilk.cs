0b2a36aef57098f544283a908c15b85417f5ca0f
NathanHarbour <nathanharbour@gmail.com>
2021-01-12 11:35:45 -0600

    Initial submission

---
+++ Source/CM_Wheres_Daddy_Going/MentalStateWorker_OutToGetSomeMilk.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace CM_Wheres_Daddy_Going
+{
+    public class MentalStateWorker_OutToGetSomeMilk : MentalStateWorker
+    {
+        public override bool StateCanOccur(Pawn pawn)
+        {
+            if (pawn == null || pawn.Dead || !pawn.Spawned || pawn.relations == null || pawn.relations.RelatedPawns == null)
+                return false;
+
+            List<Pawn> children = pawn.relations.Children.ToList();
+
+            if (children == null || children.Count == 0)
+                return false;
+
+            int childrenOnMap = children.Where(child => child.Map == pawn.Map).Count();
+
+            return childrenOnMap > 0;
+        }
+    }
+}
---
+++ Source/CM_Wheres_Daddy_Going/MentalState_OutToGetSomeMilk.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace CM_Wheres_Daddy_Going
+{
+    public class MentalState_OutToGetSomeMilk : MentalState
+    {
+        private Pawn child = null;
+        private ThingDef thingDef = null;
+
+        public override string InspectLine => string.Format(base.InspectLine, thingDef.label);
+
+        public override RandomSocialMode SocialModeMax()
+        {
+            return RandomSocialMode.Off;
+        }
+
+        public override void PreStart()
+        {
+            base.PreStart();
+
+            ParentalMemory memory = WheresDaddyGoingMod.Instance.MemoriesOfDad.GetOrCreateMemory(pawn);
+
+            List<Pawn> childrenOnMap = pawn.relations.Children.Where(child => child.Map == pawn.Map).ToList();
+            if (childrenOnMap.Count > 0)
+                child = childrenOnMap.RandomElement();
+
+            thingDef = memory.thingDef;
+            if (thingDef == null)
+            {
+                thingDef = DefDatabase<ThingDef>.GetNamed("Milk");
+                
+                if (thingDef == null)
+                    thingDef = ThingSetMaker_ResourcePod.RandomPodContentsDef(false);
+
+                memory.thingDef = thingDef;
+            }
+        }
+
+        public override string GetBeginLetterText()
+        {
+            string baseLetter = "";
+            if (child == null || pawn.gender == Gender.None)
+            {
+                baseLetter = def.beginLetter.Formatted(pawn.NameShortColored, thingDef.label, pawn.Named("PAWN")).AdjustedFor(pawn).Resolve().CapitalizeFirst();
+            }
+            else if (pawn.gender == Gender.Male)
+            {
+                baseLetter = "CM_Wheres_Daddy_Going_MentalState_OutToGetSomeMilk_Letter_Daddy".Translate(thingDef.label, child.NameShortColored);
+            }
+            else if (pawn.gender == Gender.Female)
+            {
+                baseLetter = "CM_Wheres_Daddy_Going_MentalState_OutToGetSomeMilk_Letter_Mommy".Translate(thingDef.label, child.NameShortColored);
+            }
+
+            return baseLetter;
+            //return def.beginLetter.Formatted(pawn.NameShortColored, thingDef.label, pawn.Named("PAWN")).AdjustedFor(pawn).Resolve().CapitalizeFirst();
+            //return def.beginLetter.Formatted(pawn.NameShortColored, thingDef.label, pawn.Named("PAWN"), thingDef.Named("TARGET")).AdjustedFor(pawn).Resolve().CapitalizeFirst();
+            //return def.beginLetter.Formatted(pawn.NameShortColored, target.NameShortColored, pawn.Named("PAWN"), target.Named("TARGET")).AdjustedFor(pawn).Resolve().CapitalizeFirst();
+        }
+    }
+}
